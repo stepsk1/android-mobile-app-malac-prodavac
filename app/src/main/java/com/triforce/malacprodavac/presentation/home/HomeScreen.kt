b74ed85852f74f7fa2826aaf891952a101681634
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-10-27 19:05:41 +0200

    Home screen Categories Section Composable

--- app/src/main/java/com/triforce/malacprodavac/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/home/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.triforce.malacprodavac.presentation.home
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -19,8 +22,13 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import com.triforce.malacprodavac.Screen
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,16 +39,19 @@ import com.triforce.malacprodavac.presentation.RegistrationFormEvent
 import com.triforce.malacprodavac.presentation.login.LoginFormEvent
 import com.triforce.malacprodavac.presentation.login.LoginViewModel
 import com.triforce.malacprodavac.ui.theme.MP_Green
+import com.triforce.malacprodavac.ui.theme.MP_GreenDark
+import com.triforce.malacprodavac.ui.theme.MP_GreenLight
 import com.triforce.malacprodavac.ui.theme.MP_White
 
 @Composable
 fun HomeScreen(navController: NavController) {
     Box(modifier = Modifier
-        .background(MP_Green)
+        .background(MP_White)
         .fillMaxSize()
     ){
         Column {
             GreetingSection()
+            CategoriesSection(categories = listOf("Malac Pijaca", "Profil", "Porudžbine"))
         }
     }
 }
@@ -77,5 +88,38 @@ fun GreetingSection(
             modifier = Modifier
                 .size(24.dp)
         )
+    }
+}
+
+@Composable
+fun CategoriesSection(
+    categories: List<String>
+) {
+    var selectedCategoryIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(categories.size) {
+            Box (
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedCategoryIndex = it // current index of the box
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedCategoryIndex == it)
+                            MP_GreenDark
+                        else
+                            MP_GreenLight
+                    )
+                    .padding(15.dp)
+            )
+            {
+                Text(text = categories[it], color = MP_White )
+            }
+        }
     }
 }
