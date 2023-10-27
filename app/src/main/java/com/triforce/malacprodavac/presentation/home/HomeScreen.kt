959f6667552402534752d66441138c6990f0fe97
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-10-27 23:21:04 +0200

    Home screen Recommended Features Section Composable

--- app/src/main/java/com/triforce/malacprodavac/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/home/HomeScreen.kt
@@ -4,17 +4,26 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
@@ -33,29 +42,58 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.triforce.malacprodavac.Feature
 import com.triforce.malacprodavac.presentation.RegistrationFormEvent
 import com.triforce.malacprodavac.presentation.login.LoginFormEvent
 import com.triforce.malacprodavac.presentation.login.LoginViewModel
 import com.triforce.malacprodavac.ui.theme.MP_Green
 import com.triforce.malacprodavac.ui.theme.MP_GreenDark
 import com.triforce.malacprodavac.ui.theme.MP_GreenLight
+import com.triforce.malacprodavac.ui.theme.MP_Orange
 import com.triforce.malacprodavac.ui.theme.MP_Pink
 import com.triforce.malacprodavac.ui.theme.MP_White
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Box(modifier = Modifier
-        .background(MP_White)
-        .fillMaxSize()
-    ){
+    Box(
+        modifier = Modifier
+            .background(MP_White)
+            .fillMaxSize()
+    ) {
         Column {
             GreetingSection()
             CategoriesSection(categories = listOf("Malac Pijaca", "Profil", "Porudžbine"))
-            GoToStore()
+            GoToStoreSection()
+            RecommendedFeaturesSection(
+                features = listOf(
+                    Feature(
+                        title = "Prodavnica",
+                        graphicID = Icons.Default.AddCircle,
+                        backgroundColor = MP_GreenDark
+                    ),
+                    Feature(
+                        title = "Moj Profil",
+                        graphicID = Icons.Default.AccountCircle,
+                        backgroundColor = MP_Orange
+                    ),
+                    Feature(
+                        title = "Omiljeno",
+                        graphicID = Icons.Default.Favorite,
+                        backgroundColor = MP_GreenLight
+                    ),
+                    Feature(
+                        title = "Korpa",
+                        graphicID = Icons.Default.ShoppingCart,
+                        backgroundColor = MP_Green
+                    )
+                )
+            )
         }
     }
 }
@@ -63,14 +101,14 @@ fun HomeScreen(navController: NavController) {
 @Composable
 fun GreetingSection(
     name: String = "Filip"
-){
+) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
             .padding(15.dp)
-    ){
+    ) {
 
         Column(
             verticalArrangement = Arrangement.Center
@@ -105,7 +143,7 @@ fun CategoriesSection(
 
     LazyRow {
         items(categories.size) {
-            Box (
+            Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
@@ -122,17 +160,17 @@ fun CategoriesSection(
                     .padding(15.dp)
             )
             {
-                Text(text = categories[it], color = MP_White )
+                Text(text = categories[it], color = MP_White)
             }
         }
     }
 }
 
 @Composable
-fun GoToStore(
+fun GoToStoreSection(
     color: Color = MP_Pink
 ) {
-    Row (
+    Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
@@ -141,7 +179,7 @@ fun GoToStore(
             .background(color)
             .padding(horizontal = 15.dp, vertical = 20.dp)
             .fillMaxWidth()
-    ){
+    ) {
         Column {
             Text(
                 text = "Istraži našu prodavnicu",
@@ -161,13 +199,95 @@ fun GoToStore(
                 .clip(CircleShape)
                 .background(MP_GreenDark)
                 .padding(10.dp)
-        ){
+        ) {
             Icon(
                 imageVector = Icons.Default.ShoppingCart,
                 contentDescription = "Store",
                 tint = MP_White,
                 modifier = Modifier
                     .size(16.dp)
+            )
+        }
+    }
+}
+
+@Composable
+fun RecommendedFeaturesSection(
+    features: List<Feature>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = "Istaknute Akcije",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier
+                .padding(15.dp)
+        )
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(
+                start = 7.5.dp,
+                end = 7.5.dp,
+                bottom = 100.dp
+            ), // 100 dp bottom padding because navigation
+            modifier = Modifier.fillMaxHeight(),
+        ) {
+            items(features.size) {// how many items do we have
+                // define one of items
+                RecommendedFeatureItem(feature = features[it])
+            }
+        }
+    }
+}
+
+@Composable
+fun RecommendedFeatureItem(
+    feature: Feature
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(7.5.dp)
+            .aspectRatio(1F) // ratio is 1x1 so whatever the width is, the hegiht will be the same
+            .clip(RoundedCornerShape(10.dp))
+            .background(feature.backgroundColor)
+    ) {
+        val width = constraints.maxWidth
+        val height = constraints.maxHeight
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(15.dp)
+        ) {
+            Text(
+                text = feature.title,
+                style = MaterialTheme.typography.h2,
+                lineHeight = 26.sp,
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+            )
+            Icon(
+                imageVector = feature.graphicID,
+                contentDescription = feature.title,
+                tint = MP_White,
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+            )
+            Text(
+                text = "Otvori",
+                color = MP_White,
+                fontSize = 14.sp,
+                fontWeight =  FontWeight.Bold,
+                modifier = Modifier
+                    .clickable {
+                        // we need to handle the click...
+                    }
+                    .align(Alignment.BottomEnd)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(MP_Pink)
+                    .padding(vertical = 6.dp, horizontal = 15.dp)
             )
         }
     }
