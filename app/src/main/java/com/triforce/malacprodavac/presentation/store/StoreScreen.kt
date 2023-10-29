a2a8e01f857b6674e0a8b14698f39b8fc460b482
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-10-29 18:19:16 +0100

    Store Screen New Title Text Content Section Composable, icon, text and title can be modified and can be used throughout the app. Added navigation from home screen to Store Screen.

--- app/src/main/java/com/triforce/malacprodavac/presentation/store/StoreScreen.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/store/StoreScreen.kt
@@ -1,6 +1,7 @@
 package com.triforce.malacprodavac.presentation.store
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,23 +11,37 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.triforce.malacprodavac.BottomNavigationMenuContent
 import com.triforce.malacprodavac.LinearGradient
+import com.triforce.malacprodavac.Screen
+import com.triforce.malacprodavac.presentation.home.BottomNavigationMenu
 import com.triforce.malacprodavac.presentation.home.GreetingSection
 import com.triforce.malacprodavac.ui.theme.MP_Black
+import com.triforce.malacprodavac.ui.theme.MP_Gray
+import com.triforce.malacprodavac.ui.theme.MP_Green
 import com.triforce.malacprodavac.ui.theme.MP_GreenDark
 import com.triforce.malacprodavac.ui.theme.MP_GreenLight
 import com.triforce.malacprodavac.ui.theme.MP_Orange
@@ -53,14 +68,42 @@ fun StoreScreen(navController: NavController)
 
         }
         Column {
-            HeaderSectionTitle("Malac prodavnica")
+            HeaderSectionTitle("Malac prodavnica", navController)
+            TitleTextContentSection(
+                sectionTitle = "Podržite lokalnu ekonomiju",
+                sectionText = "Pronađite najbolje proizvode od malih proizvođača. Pratite svoje omiljene proizvođače i podržite lokalnu ekonomiju!",
+                iconImage = Icons.Default.Star,
+                iconColor = MP_Green
+            )
         }
+
+        BottomNavigationMenu(
+            items = listOf(
+                BottomNavigationMenuContent(
+                    title = "Početna",
+                    graphicID = Icons.Default.Home
+                ),
+                BottomNavigationMenuContent(
+                    title = "Prodavnica",
+                    graphicID = Icons.Default.AddCircle
+                ),
+                BottomNavigationMenuContent(
+                    title = "Moj Profil",
+                    graphicID = Icons.Default.AccountCircle
+                ),
+                BottomNavigationMenuContent(
+                    title = "Korpa",
+                    graphicID = Icons.Default.ShoppingCart
+                )
+            ), modifier = Modifier.align(Alignment.BottomCenter)
+        )
     }
 }
 
 @Composable
 fun HeaderSectionTitle(
-    msg: String
+    msg: String,
+    navController: NavController,
 ) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -81,6 +124,9 @@ fun HeaderSectionTitle(
                 tint = MP_White,
                 modifier = Modifier
                     .size(30.dp)
+                    .clickable {
+                        navController.navigate(Screen.HomeScreen.route)
+                    }
             )
 
             Text(
@@ -96,6 +142,52 @@ fun HeaderSectionTitle(
             tint = MP_White,
             modifier = Modifier
                 .size(35.dp)
+        )
+    }
+}
+
+@Composable
+fun TitleTextContentSection(
+    sectionTitle: String,
+    sectionText: String,
+    iconImage: ImageVector,
+    iconColor: Color
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(25.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+
+            Text(
+                text = sectionTitle,
+                style = MaterialTheme.typography.h6,
+                color = MP_Black
+            )
+
+            Icon(
+                imageVector = iconImage,
+                contentDescription = iconImage.toString(),
+                tint = iconColor,
+                modifier = Modifier
+                    .size(35.dp)
+            )
+        }
+
+        Text(
+            text = sectionText,
+            style = MaterialTheme.typography.body1,
+            color = Color.Gray,
+            modifier = Modifier
+                .padding(top = 15.dp)
         )
     }
 }
