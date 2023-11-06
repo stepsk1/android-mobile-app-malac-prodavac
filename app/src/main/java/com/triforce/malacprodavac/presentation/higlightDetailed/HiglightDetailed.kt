c3383a2e91910549c12b95d9806e17e093656e25
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-11-06 11:27:20 +0100

    Highlight detailed added top brief desription

--- app/src/main/java/com/triforce/malacprodavac/presentation/higlightDetailed/HiglightDetailed.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/higlightDetailed/HiglightDetailed.kt
@@ -1,19 +1,29 @@
 package com.triforce.malacprodavac.presentation.higlightDetailed
 
+import android.text.Layout
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.triforce.malacprodavac.LinearGradient
@@ -23,34 +33,35 @@ import com.triforce.malacprodavac.presentation.store.HeaderSectionTitle
 import com.triforce.malacprodavac.presentation.store.category.CategorySectionHeader
 import com.triforce.malacprodavac.presentation.store.category.FilterSortRow
 import com.triforce.malacprodavac.presentation.store.category.ShowcaseProducts
+import com.triforce.malacprodavac.ui.theme.MP_Black
+import com.triforce.malacprodavac.ui.theme.MP_Green
 import com.triforce.malacprodavac.ui.theme.MP_Pink
 import com.triforce.malacprodavac.ui.theme.MP_Pink_Dark
 import com.triforce.malacprodavac.ui.theme.MP_White
 
 @Composable
-fun HighlightDetailed(navController: NavController)
-{
+fun HighlightDetailed(navController: NavController) {
     Box(
         modifier = Modifier
             .background(MP_White)
             .fillMaxSize()
-    ){
-        LinearGradient(color1 = MP_Pink, color2 = MP_Pink_Dark )
-        Surface (
-            color = MP_White,
+    ) {
+        LinearGradient(color1 = MP_Pink, color2 = MP_Pink_Dark)
+        Surface(
             modifier = Modifier
                 .fillMaxWidth()
-                .fillMaxHeight(1F)
-                .padding(top = 250.dp)
+                .fillMaxHeight()
+                .padding(top = 65.dp)
                 .clip(RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp))
-        ){
+        ) {
 
         }
         Column {
-            HeaderSectionTitle("Sirupi i sokovi", navController)
-            CategorySectionHeader("100% domaći i prirodni sokovi",
-                "Voće se prvo hladno cedi, zatim pasterizuje i bez ikakvih dodataka pakuje u staklenu ambalažu.")
-            CategoriesSection(categories = listOf("Bez Aditiva", "Sirupi", "Sokovi"))
+            HeaderSectionTitle("Više od prodavca", navController)
+            BriefDescription(
+                title = "Sveži sokovi za Vaš užitak!",
+                description = "Domaćinstvo Perun se generacijama bavi domaćom proizvodnjom sokova i sirupa od različitog voća koje se hladno cedi, zatim..."
+            )
             FilterSortRow(navController)
             ShowcaseProducts(
                 products = listOf(
@@ -99,5 +110,39 @@ fun HighlightDetailed(navController: NavController)
                 ), navController
             )
         }
+    }
+}
+
+@Composable
+fun BriefDescription(
+    title: String,
+    description: String
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth(1F)
+            .height(
+                height = 200.dp
+            )
+            .padding(
+                horizontal = 30.dp
+            )
+    ) {
+        Text(
+            text = title,
+            style = MaterialTheme.typography.h5,
+            color = MP_Black,
+            fontWeight = FontWeight.W400,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = description,
+            style = androidx.compose.material.MaterialTheme.typography.body1,
+            color = Color.Gray,
+            fontWeight = FontWeight.W300,
+            textAlign = TextAlign.Center
+        )
     }
 }
