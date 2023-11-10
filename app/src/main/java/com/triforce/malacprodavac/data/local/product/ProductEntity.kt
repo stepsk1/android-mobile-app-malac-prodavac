a73b43dab2ceda375e0a061fc0051c647f9f58c5
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-11-10 17:48:15 +0100

    Defined Product Entity

---
+++ app/src/main/java/com/triforce/malacprodavac/data/local/product/ProductEntity.kt
@@ -0,0 +1,25 @@
+package com.triforce.malacprodavac.data.local.product
+
+import androidx.room.PrimaryKey
+
+data class ProductEntity(
+    @PrimaryKey
+    val id: Int,
+    val available: Boolean,
+    val price: Double,
+    val unitOfMeasurement: String, //Convert to enum
+    val rating: Double,
+    val availableAtLatitude: Double,
+    val availableAtLongitude: Double,
+    val availableFromHours: Double,
+    val availableTillHours: Double,
+    val currency: String, //Convert to enum
+    val shopId: Int,
+    val title: String,
+    val desc: String,
+    val ratingsCount: Double,
+    val availableAt: String,
+    val categoryId: Int,
+    val createdAt: String,
+    val updatedAt: String,
+)
