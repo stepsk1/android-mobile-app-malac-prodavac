0c904542d4aa36e9c60d07b922a1da61ca435ca0
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-11-10 18:29:10 +0100

    Define product queries in Product DAO

---
+++ app/src/main/java/com/triforce/malacprodavac/data/local/product/ProductDao.kt
@@ -0,0 +1,20 @@
+package com.triforce.malacprodavac.data.local.product
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface ProductDao {
+    @Query("SELECT * FROM ProductEntity")
+    suspend fun getProducts(): List<ProductEntity>
+
+    @Query("SELECT * FROM ProductEntity WHERE id = :id")
+    suspend fun getProductForId(id: Int):ProductEntity
+
+    @Delete
+    suspend fun deleteProduct(product: ProductEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertProduct(product: ProductEntity)
+}
