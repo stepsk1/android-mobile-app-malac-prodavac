244feeed23084f5bfa24c024bf78154b0815c8f5
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-11-11 16:44:33 +0100

    Implemented Get Product Use Case

---
+++ app/src/main/java/com/triforce/malacprodavac/domain/use_case/product/GetProductForId.kt
@@ -0,0 +1,19 @@
+package com.triforce.malacprodavac.domain.use_case.product
+
+import com.triforce.malacprodavac.domain.model.Product
+import com.triforce.malacprodavac.domain.repository.ProductRepository
+import com.triforce.malacprodavac.util.Resource
+import kotlinx.coroutines.flow.Flow
+
+class GetProductForId(
+
+    private val repository: ProductRepository
+
+) {
+
+    suspend operator fun invoke(id: Int, fetchFromRemote: Boolean): Flow<Resource<List<Product>>>? {
+
+        return repository.getProductForId(id, fetchFromRemote)
+
+    }
+}
