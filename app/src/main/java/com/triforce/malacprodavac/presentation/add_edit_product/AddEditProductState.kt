0abe7dc69cf6e9de7348e4e9677fe432aa72435c
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-11-19 22:39:22 +0100

    Add & Edit product view model implementation

--- app/src/main/java/com/triforce/malacprodavac/presentation/add_edit_product/AddEditProductState.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/add_edit_product/AddEditProductState.kt
@@ -1,20 +1,23 @@
 package com.triforce.malacprodavac.presentation.add_edit_product
 
+import com.triforce.malacprodavac.domain.model.Product
 import com.triforce.malacprodavac.util.enum.Currency
 import com.triforce.malacprodavac.util.enum.UnitOfMeasurement
 
 data class AddEditProductState(
-    val productId: Int? = null,
+    val productId: Int? = -1,
+    val product: Product? = null,
+    val isLoading: Boolean = false,
 
-    val unitOfMeasurement: UnitOfMeasurement? = null,
-    val currency: Currency? = null,
-    val title: String? = "",
-    val desc: String? = "",
-    val price: Double? = null,
-    val categoryId: Int? = null,
-    val availableAt: String? = "",
-    val availableAtLatitude: Double? = null,
-    val availableAtLongitude: Double? = null,
-    val availableFromHours: Int? = null,
-    val availableTillHours: Int? = null
+    val unitOfMeasurement: UnitOfMeasurement = UnitOfMeasurement.KG,
+    val currency: Currency = Currency.RSD,
+    val title: String = "",
+    val desc: String = "",
+    val price: Double = 0.0,
+    val categoryId: Int = 0,
+    val availableAt: String = "",
+    val availableAtLatitude: Double = 90.0,
+    val availableAtLongitude: Double = 90.0,
+    val availableFromHours: Int = 0,
+    val availableTillHours: Int = 0
 )
