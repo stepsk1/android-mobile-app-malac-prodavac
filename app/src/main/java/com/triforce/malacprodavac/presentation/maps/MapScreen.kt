0df0a1c2a63670179602e3e15d1e75155e87b8e2
Filip Milosavljević <filipmilosavljevice14@gmail.com>
2023-12-03 22:37:01 +0100

    Added Navigation Bar To Map Search

--- app/src/main/java/com/triforce/malacprodavac/presentation/maps/MapScreen.kt
+++ app/src/main/java/com/triforce/malacprodavac/presentation/maps/MapScreen.kt
@@ -6,17 +6,25 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Clear
 import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
@@ -35,9 +43,12 @@ import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
+import com.triforce.malacprodavac.BottomNavigationMenuContent
 import com.triforce.malacprodavac.R
 import com.triforce.malacprodavac.Screen
+import com.triforce.malacprodavac.presentation.components.BottomNavigationMenu
 import com.triforce.malacprodavac.presentation.maps.components.BottomMapShopDetails
+import com.triforce.malacprodavac.ui.theme.MP_White
 
 @Composable
 fun MapScreen(
@@ -66,70 +77,125 @@ fun MapScreen(
 
     Scaffold(
         scaffoldState = scaffoldState,
-        floatingActionButton = {
-            FloatingActionButton(onClick = {
-                viewModel.onEvent(MapEvent.ToggleSpecialMap)
-            }) {
-                Icon(
-                    imageVector = if (viewModel.state.isSpecialMap) {
-                        Icons.Outlined.Clear
-                    } else {
-                        Icons.Outlined.LocationOn
-                    },
-                    contentDescription = "Toggle Special map"
-                )
-            }
-        },
-        content = { padding ->
-            GoogleMap(
-                /*cameraPositionState = CameraPositionState(
-                    position = CameraPosition(
-                        LatLng(44.01667, 20.91667),
-                        12.0f,
-                        0.0f,
-                        0.0f
+        /*floatingActionButton = {
+                FloatingActionButton(
+                    onClick = { viewModel.onEvent(MapEvent.ToggleSpecialMap) }
+                ) {
+                    Icon(
+                        imageVector = if (viewModel.state.isSpecialMap) {
+                            Icons.Outlined.Clear
+                        } else {
+                            Icons.Outlined.LocationOn
+                        },
+                        contentDescription = "Toggle Special map"
                     )
-                ),*/
-                properties = viewModel.state.properties,
-                uiSettings = uiSettings,
-                modifier = Modifier
-                    .padding(padding)
-                    .fillMaxSize(),
-                onMapLongClick = {
-                    viewModel.onEvent(MapEvent.OnMapLongClick(it))
                 }
+            },*/
+        content = { padding ->
+            Box(
+                modifier = Modifier.padding(padding)
             ) {
-                viewModel.state.shops!!.forEach { shop ->
-                    if (shop.availableAtLatitude != null && shop.availableAtLongitude != null) {
-                        Marker(
-                            position = LatLng(
-                                shop.availableAtLatitude,
-                                shop.availableAtLongitude
-                            ),
-                            title = shop.businessName + "user id " + shop.user?.id + " shop id " + shop.id,
-                            snippet = if (shop.user != null) {
-                                shop.user.firstName + " " + shop.user.lastName
-                            } else {
-                                ""
-                            },
-                            /*onInfoWindowLongClick = {
-                                viewModel.onEvent(MapEvent.OnInfoWindowLongClick(shop))
-                            },*/
-                            onClick = {
-                                viewModel.onEvent(MapEvent.OnInfoWindowLongClick(shop))
-                                //it.showInfoWindow()
-                                true
-                            },
-                            icon = bitmapDescriptorFromVector(
-                                LocalContext.current,
-                                R.drawable.shop_icon
-                            )
+                GoogleMap(
+                    cameraPositionState = CameraPositionState(
+                        position = CameraPosition(
+                            LatLng(44.01667, 20.91667),
+                            12.0f,
+                            0.0f,
+                            0.0f
                         )
+                    ),
+                    properties = viewModel.state.properties,
+                    uiSettings = uiSettings,
+                    modifier = Modifier
+                        .padding(padding)
+                        .fillMaxSize(),
+                    onMapLongClick = {
+                        viewModel.onEvent(MapEvent.OnMapLongClick(it))
+                    }
+                ) {
+                    viewModel.state.shops!!.forEach { shop ->
+                        if (shop.availableAtLatitude != null && shop.availableAtLongitude != null) {
+                            Marker(
+                                position = LatLng(
+                                    shop.availableAtLatitude,
+                                    shop.availableAtLongitude
+                                ),
+                                title = shop.businessName + "user id " + shop.user?.id + " shop id " + shop.id,
+                                snippet = if (shop.user != null) {
+                                    shop.user.firstName + " " + shop.user.lastName
+                                } else {
+                                    ""
+                                },
+                                /*onInfoWindowLongClick = {
+                                    viewModel.onEvent(MapEvent.OnInfoWindowLongClick(shop))
+                                },*/
+                                onClick = {
+                                    viewModel.onEvent(MapEvent.OnInfoWindowLongClick(shop))
+                                    //it.showInfoWindow()
+                                    true
+                                },
+                                icon = bitmapDescriptorFromVector(
+                                    LocalContext.current,
+                                    R.drawable.shop_icon
+                                )
+                            )
+                        }
                     }
                 }
+                FloatingActionButton(
+                    onClick = {
+                        viewModel.onEvent(MapEvent.ToggleSpecialMap)
+                    },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(16.dp)
+                ) {
+                    Icon(
+                        imageVector = if (viewModel.state.isSpecialMap) {
+                            Icons.Outlined.Clear
+                        } else {
+                            Icons.Outlined.LocationOn
+                        },
+                        contentDescription = "Toggle Special map"
+                    )
+                }
+                BottomNavigationMenu(
+                    navController = navController,
+                    items = listOf(
+                        BottomNavigationMenuContent(
+                            title = "Početna",
+                            graphicID = Icons.Default.Home,
+                            screen = Screen.HomeScreen,
+                            isActive = false
+                        ),
+                        BottomNavigationMenuContent(
+                            title = "Market",
+                            graphicID = ImageVector.vectorResource(R.drawable.logo_green),
+                            screen = Screen.StoreScreen,
+                            isActive = true
+                        ),
+                        BottomNavigationMenuContent(
+                            title = "Profil",
+                            graphicID = Icons.Default.Person,
+                            screen = Screen.PrivateProfile,
+                            isActive = false
+                        ),
+                        BottomNavigationMenuContent(
+                            title = "Korpa",
+                            graphicID = Icons.Default.ShoppingCart,
+                            screen = Screen.CartScreen,
+                            isActive = false
+                        )
+                    ), modifier = Modifier.align(Alignment.BottomCenter)
+                )
             }
+
             viewModel.state.selectedShop?.let { selectedShop ->
-                BottomMapShopDetails(selectedShop, viewModel.state.showShopDetails, navController)
+                BottomMapShopDetails(
+                    selectedShop,
+                    viewModel.state.showShopDetails,
+                    navController
+                )
             }
         }
     )
