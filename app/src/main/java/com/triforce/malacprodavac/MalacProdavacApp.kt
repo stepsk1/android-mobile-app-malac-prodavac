a4ef2eb2a6dee72f9971ecd7ea09247e64a6ec48
Nikola Milovanovic <nikolam998@gmail.com>
2023-12-10 16:59:26 +0100

    !WIP

--- app/src/main/java/com/triforce/malacprodavac/MalacProdavacApp.kt
+++ app/src/main/java/com/triforce/malacprodavac/MalacProdavacApp.kt
@@ -1,6 +1,8 @@
 package com.triforce.malacprodavac
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import coil.memory.MemoryCache
@@ -10,6 +12,18 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MalacProdavacApp : Application(), ImageLoaderFactory {
+
+    override fun onCreate() {
+        super.onCreate()
+        val notificationChannel = NotificationChannel(
+            "malac_prodavac",
+            "Malac Prodavac",
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+    }
+
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
             .memoryCache {
